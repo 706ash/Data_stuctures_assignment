5550e2d8695f57816fe6deaf3411359bb1a7a63c
706ash <114875724+706ash@users.noreply.github.com>
2023-10-16 16:50:14 +0530

    update bst.c

--- datastructures/bst.c
+++ datastructures/bst.c
@@ -41,18 +41,57 @@ void preorder(struct node *root)
         preorder(root->right);
     }    
 }
+
+void inorder(struct node *root)
+{
+    if(root!=NULL)
+    {
+        preorder(root->left);
+        printf("%d\t",root->data);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct node *root)
+{
+    if(root!=NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d\t",root->data);
+    }
+}
 int main()
 {
-    int data,child,con;
+    int data,child,con,o;
     struct node *root=NULL;
     do{
-    printf("\nEnter data to be inserted in bst: ");
-    scanf("%d",&child);
-    root=insert(child,root);
+        printf("\nEnter 1 to insert data in the binary search tree\nEnter 2 to display the tranversals\n");
+        scanf("%d",&o);
+        switch (o)
+        {
+        case 1:
+                printf("\nEnter data to be inserted in bst: ");
+                scanf("%d",&child);
+                root=insert(child,root);
+            break;
+        case 2:
+                printf("\nPreorder: ");
+                preorder(root);
+                printf("\nInorder: ");
+                inorder(root);
+                printf("\nPostorder: ");
+                postorder(root);
+                break;
+
+        
+        default:printf("\nWrong Choice");
+            break;
+        }
+   
     printf("\nDo you want to continue(1/0): ");
     scanf("%d",&con);
     }
     while(con==1);
-    preorder(root);
     return 0;
 }
