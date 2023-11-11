15afdd39d806dc68069b214aa627625f0286ef12
706ash <114875724+706ash@users.noreply.github.com>
2023-11-11 23:13:13 +0530

    update exp8

--- datastructures/exp8.c
+++ datastructures/exp8.c
@@ -4,7 +4,14 @@ void display(int m)
 {
     for(int i=0;i<m;i++)
     {
-        printf("%d\t",hash[i]);
+        if(hash[i]==0)
+        {
+            printf("h[%d]=\n",i);
+        }
+        else{
+            
+        printf("h[%d]=%d\n",i,hash[i]);
+        }
     }
 }
 void division(int *keys,int n)
@@ -16,7 +23,44 @@ void division(int *keys,int n)
         hash[keys[i]%m]=keys[i];
     }
     display(m);
+    for(int i=0;i<n;i++)
+    {
+        hash[keys[i]%m]=0;
+    }
+}
 
+void trunication(int *keys,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        hash[keys[i]/10]=keys[i];
+    }
+    display(8);
+    for(int i=0;i<n;i++)
+    {
+        hash[keys[i]/10]=0;
+    }
+}
+
+void folding(int *keys,int n)
+{
+    int sum=0,temp;
+    for(int i=0;i<n;i++)
+    {
+        temp=keys[i];
+        while(temp!=0)
+        {
+            sum+=temp%10;
+            temp=temp/10;
+        }
+        hash[sum]=keys[i];
+        sum=0;
+    }
+    display(10);
+    for(int i=0;i<n;i++)
+    {
+        hash[keys[i]/10]=0;
+    }
 }
 
 
@@ -41,14 +85,15 @@ int main()
             division(&keys[0],n);
             break;
         case 2:
-            //trunication();
+            trunication(&keys[0],n);
             break;
         case 3:
             //midsquare();
             break;
         case 4:
-            //folding();
-        default:
+            folding(&keys[0],n);
+            break;
+        default:36 18 72 43 6
             printf("\nWrong choice");
             break;
         }
