9b1b49250317c556ece7b0a388bb2e744e67964c
706ash <114875724+706ash@users.noreply.github.com>
2023-11-14 23:59:14 +0530

    update

---
+++ OOPs IT3 programs/q12022.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+template <class t>
+void swap(t *a,t *b)
+{
+    t temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+int main()
+{
+    int a,b;
+    float c,d;
+    cout<<"\nEnter integer values of a and b to be swapped: ";
+    cin>>a>>b;
+    swap(&a,&b);
+    cout<<"\nAfter swapping the value of a is "<<a<<" b is"<<b; 
+    cout<<"\nEnter floating values of c and d to be swapped: ";
+    cin>>c>>d;
+    swap(c,d);
+    cout<<"\nAfter swapping the value of c is "<<c<<" d is"<<d;
+    return 0;
+}
